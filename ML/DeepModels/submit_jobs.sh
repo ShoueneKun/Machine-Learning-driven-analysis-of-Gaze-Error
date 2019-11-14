c0b3fa0a2fff1f99eb82783285d79b02d08fd8f7
RSKothari <rsk3900@rit.edu>
2019-11-13 19:52:48 -0500

    Run both models at the same time

--- ML/DeepModels/submit_jobs.sh
+++ ML/DeepModels/submit_jobs.sh
@@ -18,6 +18,6 @@ declare -a PrTest=("1" "2" "3" "8" "9" "12" "16" "17" "22")
 for PrIdx in "${PrTest[@]}"
 do
     echo "Submitting $PrIdx"
-    echo -e "#!/bin/bash \n python3 main.py --PrTest=${PrIdx} --model=${model_num} --lr=1e-5 --modeltype=1 --batchsize=32 --epochs=1000" > command.lock
+    echo -e "#!/bin/bash \n python3 main.py --PrTest=${PrIdx} --model=${model_num} --lr=1e-5 --modeltype=1 --batchsize=32 --epochs=1000 &\n python3 main.py --PrTest=${PrIdx} --model=${model_num} --lr=1e-5 --modeltype=1 --batchsize=32 --epochs=1000 &" > command.lock
     sbatch -J ${baseJobName} --output="rc_log/${PrIdx}.o" --error="rc_log/${PrIdx}.e" --mem=16G -n 1 -t 2-4:0:0 -p tier3 -A riteyes --gres=gpu:p4:1 --mail-user=rsk3900@rit.edu --mail-type=ALL command.lock
 done
