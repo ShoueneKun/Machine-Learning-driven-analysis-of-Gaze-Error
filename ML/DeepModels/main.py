1261e9f9d985269df412d7022fc46d9d74cdfe37
RSKothari <rsk3900@rit.edu>
2019-11-15 14:39:39 -0500

    minor

--- ML/DeepModels/main.py
+++ ML/DeepModels/main.py
@@ -12,7 +12,7 @@ import pickle
 import datetime
 from args import parse_args
 from opts import train, test
-from models import model_1, model_2
+from models import model_1, model_2, model_3
 from tensorboardX import SummaryWriter
 from DataLoader import splitdata, GIW_readChunk, GIW_readSeq
 
@@ -26,6 +26,9 @@ if __name__=='__main__':
     elif args.modeltype == 2:
         print('Model 2. Dense->GRU')
         model = model_2
+    elif args.modeltype == 3:
+        print('Model 3. GIW journal. Forward.')
+        model = model_3
 
     f = open(os.path.join(args.path2data, 'Data.pkl'), 'rb')
     chunk, seq = pickle.load(f)
